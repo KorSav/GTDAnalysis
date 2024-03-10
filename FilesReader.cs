ca9579c7a94d554115e6fb7bfca00f2a529cd472
Savelii Korniiko <saveliykorniko@gmail.com>
2024-03-10 16:48:11 +0200

    General file reading

---
+++ FilesReader.cs
@@ -0,0 +1,35 @@
+﻿using CsvHelper.Configuration;
+using CsvHelper;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataExtractor
+{
+    public static class FilesReader
+    {
+        public static List<GeneralRecord> ReadGeneral( string filePath )
+        {
+            List<GeneralRecord> res;
+            var config = new CsvConfiguration(CultureInfo.InvariantCulture) {
+                ReadingExceptionOccurred = context =>
+                {
+                    if ( context.Exception is CsvHelper.TypeConversion.TypeConverterException ) {
+                        return false;
+                    }
+                    return true;
+                }
+            };
+            using var reader = new StreamReader(filePath);
+            using ( var csv = new CsvReader(reader, config) ) {
+                var records = csv.GetRecords<GeneralRecord>();
+                res = records.ToList();
+            };
+            return res;
+        }
+        
+    }
+}
---
+++ GeneralRecord.cs
@@ -0,0 +1,52 @@
+﻿using CsvHelper.Configuration.Attributes;
+
+namespace DataExtractor
+{
+    [Delimiter(",")]
+    public class GeneralRecord
+    {
+        [Name("eventid")]
+        public string EventID { get; set; }
+
+        [Name("iyear")]
+        public int Year { get; set; }
+
+        [Name("imonth")]
+        public int Month { get; set; }
+
+        [Name("iday")]
+        public int Day { get; set; }
+
+        [Name("country")]
+        public int CountryCode { get; set; }
+
+        [Name("country_txt")]
+        public string CountryName { get; set; }
+
+        [Name("region")]
+        public int RegionCode { get; set; }
+
+        [Name("region_txt")]
+        public string RegionName { get; set; }
+
+        [Name("suicide")]
+        [BooleanTrueValues("1")]
+        [BooleanFalseValues("0")]
+        public bool IsSuicide { get; set; }
+
+        [Name("gname")]
+        public string GroupName { get; set; }
+
+        [Name("nperps")]
+        public int TerroristsCount { get; set; }
+
+        [Name("weaptype1_txt")]
+        public string WeaponTypeName { get; set; }
+
+        [Name("nkill")]
+        public int DeathCount { get; set; }
+
+        [Name("nwound")]
+        public int WoundCount { get; set; }
+    }
+}
